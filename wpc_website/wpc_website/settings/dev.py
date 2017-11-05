586f31db48e4c464bf79efecbe7f1a3027ff1fbf
Navin Karkera <navin@disroot.org>
2017-11-05 20:03:13 +0530

    Setup wagtail project with environment variables.

---
+++ wpc_website/wpc_website/settings/dev.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import, unicode_literals
+
+from .base import *  # noqa
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = env('DEBUG')
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env('SECRET_KEY')
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+try:
+    from .local import *
+except ImportError:
+    pass
