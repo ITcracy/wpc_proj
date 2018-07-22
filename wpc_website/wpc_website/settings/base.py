bcaa9ac438ef74ee0eb8623c2d240f535c325899
Navin Karkera <navin@disroot.org>
2018-07-22 19:59:42 +0530

    Image render fix and improved deployment
    - Fixes image render issues in portfolio and blog index pages
    - Makes improvements in fabric file.
    
    Signed-off: Navin Karkera <navin@disroot.org>

--- wpc_website/wpc_website/settings/base.py
+++ wpc_website/wpc_website/settings/base.py
@@ -33,6 +33,13 @@ SECRET_KEY = env('SECRET_KEY')
 
 ALLOWED_HOSTS = env('ALLOWED_HOSTS').split(',')
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+
 WAGTAIL_APPS = [
     'home',
     'blog',
--- wpc_website/wpc_website/settings/dev.py
+++ wpc_website/wpc_website/settings/dev.py
@@ -3,11 +3,10 @@ from __future__ import absolute_import, unicode_literals
 from .base import *  # noqa
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = True
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 try:
     from .local import *
