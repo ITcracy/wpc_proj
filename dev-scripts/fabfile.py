4e5501deabc5e62aa184e530f45aa80548001d75
Navin Karkera <navin@disroot.org>
2018-10-11 01:49:45 +0530

    FABRIC IMPROVEMENTS
    - Adds steps in fabric to automate process of installing
      prerequisites to the server
    - Also adds step to install docker
    - Adds steps to upload gunicorn service and nginx files
      required for deployment
    
    Signed-off by: Navin Karkera <navin@disroot.org>

--- dev-scripts/fabfile.py
+++ dev-scripts/fabfile.py
@@ -2,8 +2,8 @@ import base64
 import os
 import uuid
 
-from fabric.api import cd, env, lcd, local, put, run
-from fabric.contrib.files import append, exists
+from fabric.api import cd, env, lcd, local, put, run, settings, sudo
+from fabric.contrib.files import append, exists, upload_template
 
 REPO_URL = 'https://github.com/Codennovation/wpc_proj.git'
 
@@ -20,6 +20,68 @@ def h_exists(*args, **kwargs):
         return os.path.exists(*args, **kwargs)
     else:
         return exists(*args, **kwargs)
+
+
+def _prerequisites():
+    if env.host not in ('localhost', '127.0.0.1'):
+        with settings(warn_only=True):
+            nginx = run("nginx -v")
+        if nginx.failed:
+            sudo("apt-get install python3-pip python3-dev nginx git")
+            sudo("pip3 install pipenv")
+            return True
+        else:
+            return False
+
+
+def install_docker():
+    """
+    Installs docker on the remote server and adds docker to sudoers group.
+    Should be called separately from deploy for the first time.
+    """
+    sudo("apt-get update")
+    sudo("apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common")
+    run("curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -"
+        )
+    command = 'add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) \
+    stable"'
+
+    sudo(command)
+    sudo("apt-get update")
+    sudo("apt-get install docker-ce")
+    sudo("groupadd docker")
+    sudo("usermod -aG docker $USER")
+
+
+def _upload_installation_files():
+    gunicorn_data = {"username": env.user, "venv_path": run("pipenv --venv")}
+    upload_template(
+        "gunicorn.service",
+        "/etc/systemd/system/gunicorn.service",
+        context=gunicorn_data,
+        template_dir=".",
+        use_sudo=True)
+    nginx_data = {"hostname": env.host, "username": env.user}
+    upload_template(
+        "wpc_proj",
+        "/etc/nginx/sites-available/wpc_proj",
+        context=nginx_data,
+        template_dir=".",
+        use_sudo=True)
+
+
+def _file_commands():
+    sudo("systemctl start gunicorn")
+    sudo("systemctl enable gunicorn")
+    sudo("ln -s /etc/nginx/sites-available/wpc_proj /etc/nginx/sites-enabled")
+    sudo("systemctl restart nginx")
+    sudo("ufw allow 'Nginx Full'")
 
 
 def h_append(*args, **kwargs):
@@ -46,6 +108,7 @@ def deploy():
 
 
 def _all_process():
+    new = _prerequisites()
     _get_latest_source()
     _update_virtualenv()
     if env.host in ('localhost', '127.0.0.1'):
@@ -55,6 +118,11 @@ def _all_process():
     _run_docker_compose()
     _update_static_files()
     _update_database()
+    if new:
+        _upload_installation_files()
+        _file_commands()
+    else:
+        sudo("systemctl restart gunicorn")
 
 
 def _get_latest_source():
